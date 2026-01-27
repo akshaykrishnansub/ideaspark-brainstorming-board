a993df3f31f75f4d1f584e7101d91c86abc06030
unknown <akshaykrish533516@gmail.com>
2026-01-27 11:17:22 +0530

    Changes made to component code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,31 @@
-function Navbar(){
+function Navbar({
+  showLogin=true,
+  showSignup=true,
+  rightSlot,
+  title="IdeaSpark"
+}){
     return(
         <nav className="bg-gray-700 flex px-6 py-3 justify-between items-center">
       
       {/* LEFT */}
       <h1 className="font-bold text-white text-lg">
-        Ideaspark
+        {title}
       </h1>
 
       {/* RIGHT */}
       <div className="flex items-center gap-7">
-        <a href="#" className="text-white hover:font-bold">
+        {rightSlot ? rightSlot :null}
+        {showLogin ? (
+          <a href="#" className="text-white hover:font-bold">
           Login
         </a>
+        ) : null}
 
+        {showSignup ? (
         <button className="bg-amber-600 px-3 py-2 rounded hover:bg-amber-800 text-white transition">
           Sign Up for Free
         </button>
+        ): null}
       </div>
 
     </nav>
