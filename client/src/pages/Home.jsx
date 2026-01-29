430a8b03b6fa47980d9b903f42654d1b1087d0ff
unknown <akshaykrish533516@gmail.com>
2026-01-29 09:45:27 +0530

    Added 2 more sections to Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         <h1 className='font-bold text-4xl text-white'>Why IdeaSpark?</h1>
         <div className='p-10 flex justify-center gap-3.5'>
             <div className='bg-amber-50 p-5 rounded'>
-                <h1 className='text-3xl font-bold'>The Problems</h1>
+                <h2 className='text-3xl font-bold'>The Problems</h2>
                 <ul className='p-3'>
                     <li>Ideas get lost in notes, chats, and random documents</li>
                     <li>No clear structure for brainstorming</li>
@@ -24,7 +24,7 @@ const Home = () => {
                 </ul>
             </div>
             <div className='bg-amber-50 p-5 rounded'>
-                <h1 className='text-3xl font-bold'>The Solutions</h1>
+                <h2 className='text-3xl font-bold'>The Solutions</h2>
                 <ul className='p-3'>
                     <li>🧩 Visualization using Brainstorming Boards made possible</li>
                     <li>👥 Real-Time Team Collaboration has been enabled</li>
@@ -34,8 +34,22 @@ const Home = () => {
             </div>
         </div>
     </section>
-    <section>
-
+    <section className="p-10 text-center">
+        <h2 className='text-3xl font-bold p-4'>What You Can Do with IdeaSpark</h2>
+        <div className='p-6 text-2xl'>
+            <p className='p-2'>🧠 <span className='font-bold'>Smart Brainstorming</span> - Create boards to visually map your ideas and connect related thoughts.</p>
+            <p className='p-2'>👥 <span className='font-bold'>Team Collaboration</span> - Invite teammates, build together in real time.</p>
+            <p className='p-2'>🏷️ <span className='font-bold'>Categorization</span> - Organize ideas by categories.</p>
+        </div>
+    </section>
+    <section className='bg-blue-950 p-10'>
+        <h2 className='text-center text-3xl text-white font-bold pb-4'>How IdeaSpark Works</h2>
+        <div className='bg-amber-200 p-1 pb-2'>
+            <p className='text-center text-2xl p-2'><span className="font-bold">1️⃣ Create a Board</span> - Start a new brainstorming board for your project or topic.</p>
+            <p className='text-center text-2xl p-2'><span className="font-bold">2️⃣ Add Ideas</span> - Drop in thoughts, notes, or suggestions as cards.</p>
+            <p className='text-center text-2xl p-2'><span className="font-bold">3️⃣ Organize & Tag</span> - Group ideas, add labels, and prioritize.</p>
+            <p className='text-center text-2xl p-2'><span className="font-bold">4️⃣ Collaborate & Execute</span> - Share with your team and turn ideas into action.</p>
+        </div>
     </section>
     </>
   )
