f2fbf034c4628ba4f855703b22891d43699c9cbd
unknown <akshaykrish533516@gmail.com>
2026-02-04 17:22:29 +0530

    Built Board UI

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Board = () => {
   return (
-    <div>Board</div>
+    <>
+    <title>IdeaSpark | Boards</title>
+    <Navbar
+    showLogin={false} showSignup={false}
+    rightSlot={
+      <>
+      <button className='bg-green-600 p-2 rounded'>Save Board</button>
+      <button className='bg-amber-600 p-2 rounded'>Logout</button>
+      </>
+    }
+    />
+    <aside className='w-64 bg-blue-950 min-h-screen'>
+      <div className='text-white pt-4 px-2 font-bold text-2xl'>Name of the Board</div>
+      <div className='text-white font-bold pt-4 px-2 text-2xl'>Category<br/>----------------------</div>
+      <div className='text-white px-2 hover:text-amber-600 cursor-pointer'>Add a Category</div>
+      <div className='px-2 pt-6 font-extrabold text-white text-3xl'>Invite Members to the board</div>
+    </aside>
+    </>
   )
 }
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -12,12 +12,16 @@ const Dashboard = () => {
     rightSlot={
     <button className='bg-amber-600 p-2 rounded hover:bg-amber-700 transition-colors'>Logout</button>
     }/>
-    <aside className='bg-blue-950 w-64 min-h-screen justify-center px-2'>
+    <div className="flex">
+      <aside className='bg-blue-950 w-64 h-screen justify-center px-2 min-h-screen'>
         <div className='text-black pt-2 font-bold'>MENU</div>
         <div className='text-white pt-3 font-bold'><Link to='/boards'>Create New Board</Link></div>
-        <div className='text-white pt-3 font-bold'>My Boards</div>
         <div className='text-white pt-3 font-bold'>My Profile</div>
-    </aside>
+      </aside>
+      <main className='p-2 flex-1 overflow-y-auto'>
+        
+      </main>
+    </div>  
     </>
   )
 }
