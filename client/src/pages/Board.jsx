4748c1e8a32b2b95134a52ac35a73badbae72c4d
unknown <akshaykrish533516@gmail.com>
2026-02-05 21:09:44 +0530

    Frontend done for creating  Boards

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -34,18 +34,18 @@ const Board = () => {
       <div className='text-white px-2 hover:text-amber-600 cursor-pointer'>Add a Category</div>
       <div className='px-2 pt-6 font-extrabold text-white text-3xl'>Invite Members to the board</div>
       </aside>
-      <main className='flex-1'>
-        <p>Hello</p>
+      <main className='flex-1 bg-blue-900 h-14 text-3xl text-center p-2 text-white font-extrabold'>
+        {savedBoardName || 'No board created yet'}
       </main>
   </div>
   {showModal?(
     <div className='fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center'>
       <div className='bg-white w-100 p-10 border rounded-lg mb-4'>
         <label htmlFor="boardName" className='text-center font-bold'>Enter the name of the board here:</label>
-        <input type="text" className='w-full p-2 mt-4 border' value={boardName} />
+        <input type="text" className='w-full p-2 mt-4 border' value={boardName} onChange={(e)=>setBoardName(e.target.value)} />
         <div className='flex gap-2'>
-          <button className='bg-blue-500 p-2 mt-4 text-white'>Add Name</button>
-          <button className='bg-red-700 p-2 mt-4 text-white' onClick={()=>setShowModal(false)}>Cancel</button>
+          <button className='bg-blue-500 p-2 mt-4 text-white rounded' onClick={handleSaveBoardName}>Add Name</button>
+          <button className='bg-red-700 p-2 mt-4 text-white rounded' onClick={()=>setShowModal(false)}>Cancel</button>
         </div>
       </div>
     </div>
